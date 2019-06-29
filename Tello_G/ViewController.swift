475890e26ee7421e2041a879c1f9ff49c2f9d4a0
LU, CHUN-AN <cat605101@yahoo.com.tw>
2019-06-29 23:45:53 +0800

    add socket & func(send & recv)

--- Tello_G/ViewController.swift
+++ Tello_G/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
     var csv = [[String]]()
     var handle = 1
     var tello_Num = 0
+    var tello = [UDPClient]()
+    let port = 8889
+    var data = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +34,10 @@ class ViewController: UIViewController {
         for i in line{
             csv.append(i.components(separatedBy: ","))
         }
-
         print(csv)
-        //timer
         
+        //創建tello 的 socket陣列
+        create_Tello_UDP()
     }
     //======================= timer ==========================
     //timer開始
@@ -72,14 +75,58 @@ class ViewController: UIViewController {
             }
         }
     }
-    //=======================================
+    //=============== socket ===============
+    func calc_Tello_Num(){
+        tello_Num = csv[0].count - 1
+        print(tello_Num)
+    }
+    func create_Tello_UDP(){
+        calc_Tello_Num()
+        
+        for i in 1...tello_Num{
+            tello.append(UDPClient(address: csv[0][i], port: Int32(port), myAddresss: "", myPort: Int32(port)))
+        }
+    }
+    
+    func close_Tello_UDP(){
+        calc_Tello_Num()
+        for i in 1...tello_Num{
+            tello[i].close()
+        }
+    }
+    //============== sned & recv ==============
+    func send(_ s: [String]){
+        for i in 1...tello_Num{
+            _ = tello[i - 1].send(string: s[i])
+        }
+    }
+    
+    func recvData(){
+        for i in 1...tello_Num{
+            data.append("")
+            
+            let queue = DispatchQueue(label: "com.nkust.tello" + String(i))//宣告 label需要唯一性
+            queue.async {
+                while true{
+                    let s = self.tello[i - 1].recv(20)//最多接收20
+                    self.data[i-1] = self.get_String_Data(s.0!)
+                    print("Tello" + String(i) + "recv:" + self.data[i-1])
+                }
+            }
+        }
+    }
+    func get_String_Data(_ data: [Byte]) -> (String){
+        let string1 = String(data: Data(data), encoding: .utf8) ?? ""
+        return string1
+    }
+    //================== BT =====================
     @IBAction func timerStart(_ sender: Any) {
         timerStart()
     }
-    
     //=================================================
     override func viewDidDisappear(_ animated: Bool) {
         timerStop()
+        close_Tello_UDP()
     }
 }
 
