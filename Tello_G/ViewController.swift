eb65f4143bb12fbdfd51b29dbba7607425d78a34
LU, CHUN-AN <cat605101@yahoo.com.tw>
2019-06-29 20:40:06 +0800

    add read csv

--- Tello_G/ViewController.swift
+++ Tello_G/ViewController.swift
@@ -4,21 +4,29 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var audioPlayer: AVAudioPlayer!
+    var csv = [[String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        let url = Bundle.main.url(forResource: "music", withExtension: "mp3")
+        //play music
+        let musicUrl = Bundle.main.url(forResource: "music", withExtension: "mp3")
         do {
-            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer = try AVAudioPlayer(contentsOf: musicUrl!)
             audioPlayer.prepareToPlay()
         } catch {
             print("Error:", error.localizedDescription)
         }
         audioPlayer.play()
+        
+        //read csv
+        let csvUrl = Bundle.main.url(forResource: "TelloEDU_Charlie_Puth_Marvin_Gaye ", withExtension: "csv")
+        let line = try! String(contentsOf: csvUrl!).components(separatedBy: "\r\n")
+        for i in line{
+            csv.append(i.components(separatedBy: ","))
+        }
+
+        print(csv)
     }
-
-
 }
 
