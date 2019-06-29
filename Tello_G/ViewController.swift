cf1f5aff14784014f4489a6b359d5b92391b94e4
LU, CHUN-AN <cat605101@yahoo.com.tw>
2019-06-29 19:33:39 +0800

    Initial Commit

---
+++ Tello_G/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tello_G
+//
+//  Created by 李岱米 on 2019/6/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
