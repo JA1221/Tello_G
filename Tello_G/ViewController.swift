263b1a5c91b1384c713a36f9144efb9cdf58fa15
LU, CHUN-AN <cat605101@yahoo.com.tw>
2019-09-09 14:26:30 +0800

    update notes
    1.調整註解
    2. 1...tello_Num -> 0..<tello_Num

--- Tello_G/ViewController.swift
+++ Tello_G/ViewController.swift
@@ -9,35 +9,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var logTextView: UITextView!
     @IBOutlet weak var safeSegment: UISegmentedControl!
     
-    
+    //預設飛行速度
     let defaultSpeedValue = 50
-//timer計時器
+    
+    //timer計時器
     var timer: Timer?
     var t = 0.0
-//音樂
+    
+    //音樂
     let music_FileName = "Ed Sheeran - Beautiful People"
     var audioPlayer: AVAudioPlayer!
-//csv處理
+    
+    //csv處理
     let csv_FileName = "Stepsheet"
-    var csv = [[String]]()
+    var csv = [[String]]()//CSV 主要資料陣列
     var handle = 1 //處理第幾行
-//tello Socket
-    let default_socket = UDPClient(address: "192.168.10.1", port: 8889, myAddresss: "", myPort: 8889)
+    
+    //tello Socket
+    let default_socket = UDPClient(address: "192.168.10.1", port: 8889, myAddresss: "", myPort: 8889)//預設單機連接用
     var tello_Num = 0
-    var tello = [UDPClient]() //UDP 通訊陣列
+    var tello = [UDPClient]() //群飛用 UDP 通訊陣列
     let port = 8889 //Tello 接收端口
     let sendPort_1st = 60000 // 發送端口起始編號 ex. 1:6000, 2:6001, 3: 6002
-    var data = [String]()
+    var data = [String]()//回傳資料
     
 //==================== 畫面載入 ==========================
     override func viewDidLoad() {
         super.viewDidLoad()
         UIApplication.shared.isIdleTimerDisabled = true //螢幕恆亮不休眠
-    //圓角
+       
+        //圓角
         timeLabel.layer.cornerRadius = 10
         logTextView.layer.cornerRadius = 10
         
-    //prepare music
+        //prepare music
         let musicUrl = Bundle.main.url(forResource: music_FileName, withExtension: "mp3")
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: musicUrl!)
@@ -46,21 +51,21 @@ class ViewController: UIViewController {
             print("Error:", error.localizedDescription)
         }
         
-    //read csv ＆ 存成二維陣列
+        //read csv ＆ 存成二維陣列
         let csvUrl = Bundle.main.url(forResource: csv_FileName, withExtension: "csv")
         let content = try! String(contentsOf: csvUrl!)
         csv = csv_To_Array(content)
         print(csv)
         
-    //將預設檔案寫入資料夾
+        //將預設檔案寫入資料夾
         saveFile(source: csvUrl!, destination: nil, fileName: "default.csv")
         saveFile(source: musicUrl!, destination: nil, fileName: "default.mp3")
         
-    //創建tello 的 socket陣列
+        //創建tello 的 socket陣列
         create_Tello_UDP()
         recvData()
     }
-    //==================== 檔案處理 ==========================
+//==================== 檔案處理 ==========================
     func saveFile(source: URL, destination: URL?, fileName: String){
         var dest = destination
         if dest==nil{
@@ -80,11 +85,12 @@ class ViewController: UIViewController {
         }
     }
 //======================= timer ==========================
-//timer開始
+    //timer開始
     func timerStart(){
-        //
+        //安全模式 禁止更動
         safeSegment.isEnabled = false
-        //接收資料區清空 ＆ 處理
+        
+        //接收資料區清空 ＆ 執行處理
         data_clear()
         timeHandle()
         
@@ -96,12 +102,14 @@ class ViewController: UIViewController {
         })
     }
     
-//結束timer
+    //結束timer
     func timerStop(){
-    //
+    //安全模式 開放更動
         safeSegment.isEnabled = true
+        
     //switch 關
         flyBt.setOn(false, animated: true)
+        
     //停止音樂並關歸零
         audioPlayer.stop()
         audioPlayer.currentTime = 0
@@ -109,13 +117,15 @@ class ViewController: UIViewController {
         if timer != nil{//當timer存在時 廢止
             timer?.invalidate()
             timer = nil
-        //初始化
+            
+            //初始化
             t = 0.0
             handle = 1
             self.timeLabel.text = "Time : " + String(self.t) + "s"
         }
     }
-//timer處理
+    
+    //timer處理
     func timeHandle(){
         if Double(csv[handle][0]) == t{//秒數到指令設定的秒數 執行
             print(csv[handle])
@@ -133,6 +143,7 @@ class ViewController: UIViewController {
                             send("land")
                             timerStop()
                             return
+                            
                         //個體迫降
                         }else if safeSegment.selectedSegmentIndex == 2{
                             if data[i] == "landed"{//已降落 忽略
@@ -155,6 +166,7 @@ class ViewController: UIViewController {
                                 timerStop()
                             }
                         }
+                        
                     //安全 可執行指令前清空接收區
                     }else{
                         data[i] = ""
@@ -171,42 +183,53 @@ class ViewController: UIViewController {
         }
     }
 //=============== socket ===============
-    func calc_Tello_Num(){//計算tello數量
+    //計算tello數量
+    func calc_Tello_Num(){
         tello_Num = csv[0].count - 1
     }
     
-    func create_Tello_UDP(){//創建udp socket
+    //創建udp socket
+    func create_Tello_UDP(){
         close_Tello_UDP()
         calc_Tello_Num()
         
-        for i in 1...tello_Num{
-            tello.append(UDPClient(address: csv[0][i], port: Int32(port), myAddresss: "", myPort: Int32(sendPort_1st + i)))
+        for i in 0..<tello_Num{
+            tello.append(UDPClient(address: csv[0][i + 1], port: Int32(port), myAddresss: "", myPort: Int32(sendPort_1st + i)))
         }
     }
     
-    func close_Tello_UDP(){//關閉 udp socket
+    //關閉 udp socket
+    func close_Tello_UDP(){
         //關閉socket
         for i in 0..<tello.count{
             tello[i].close()
         }
+        
         //將Tello 陣列初始化
         tello = [UDPClient]()
     }
 //============== sned & recv ==============
-    func send(_ s: [String]){//傳送String陣列給 所有無人機
-        for i in 1...tello_Num{
-            _ = tello[i - 1].send(string: s[i])
+    //傳送String陣列給 所有無人機
+    func send(_ s: [String]){
+        for i in 0..<tello_Num{
+            _ = tello[i].send(string: s[i + 1])
         }
     }
-    func send(_ s: String){//傳送單一指令給 所有無人機
-        for i in 1...tello_Num{
-            _ = tello[i - 1].send(string: s)
+    
+    //傳送單一指令給 所有無人機
+    func send(_ s: String){
+        for i in 0..<tello_Num{
+            _ = tello[i].send(string: s)
         }
     }
-    func send(_ n:Int, _ s: String){//傳送單一指令給 指定無人機
+    
+    //傳送單一指令給 指定無人機
+    func send(_ n:Int, _ s: String){
             _ = tello[n].send(string: s)
     }
-    func recvData(){//接收資料 多執行緒
+    
+    //接收資料 多執行緒
+    func recvData(){
         data = [String]() //接收區大小重設
         
         for i in 0..<tello.count{
@@ -216,7 +239,7 @@ class ViewController: UIViewController {
             queue.async {
                 while true{
                     print(String(i + 1) + " is listening.")
-                    let s = self.tello[i].recv(20)//接收 最多20字元data
+                    let s = self.tello[i].recv(20)//等待接收 最多20字元data
                     if s.0==nil{break}//被強制結束 跳出
                     
                     self.data[i] = self.get_String_Data(s.0!)//儲存
@@ -226,29 +249,32 @@ class ViewController: UIViewController {
             }
         }
     }
-    func get_String_Data(_ data: [Byte]) -> (String){//轉換陣列->String
+    
+    //轉換陣列->String
+    func get_String_Data(_ data: [Byte]) -> (String){
         var string1 = String(data: Data(data), encoding: .utf8) ?? ""
         string1 = string1.trimmingCharacters(in: .newlines)
         return string1
     }
     
-    func data_clear(){//清空接收的data
+    //清空接收的data
+    func data_clear(){
         for i in 0..<data.count{
             data[i] = ""
         }
     }
 //================== BT =====================
 
-    @IBAction func command(_ sender: Any) {
+    @IBAction func command(_ sender: Any) {//連接
         send("command")
     }
-    @IBAction func takeoff(_ sender: Any) {
+    @IBAction func takeoff(_ sender: Any) {//起飛
         send("takeoff")
     }
-    @IBAction func land(_ sender: Any) {
+    @IBAction func land(_ sender: Any) {//降落
         send("land")
     }
-    @IBAction func emergency(_ sender: Any) {
+    @IBAction func emergency(_ sender: Any) {//緊急停止螺旋槳
         send("emergency")
     }
     
@@ -268,37 +294,40 @@ class ViewController: UIViewController {
         send("stop")
     }
     
-    @IBAction func forward(_ sender: Any) {
+    @IBAction func forward(_ sender: Any) {//前
         send("forward " + String(defaultSpeedValue))
     }
-    @IBAction func back(_ sender: Any) {
+    @IBAction func back(_ sender: Any) {//後
         send("back " + String(defaultSpeedValue))
     }
-    @IBAction func right(_ sender: Any) {
+    @IBAction func right(_ sender: Any) {//右
         send("right " + String(defaultSpeedValue))
     }
-    @IBAction func left(_ sender: Any) {
+    @IBAction func left(_ sender: Any) {//左
         send("left " + String(defaultSpeedValue))
     }
     
-    @IBAction func up(_ sender: Any) {
+    @IBAction func up(_ sender: Any) {//上升
         send("up " + String(defaultSpeedValue))
     }
-    @IBAction func down(_ sender: Any) {
+    @IBAction func down(_ sender: Any) {//下降
         send("down " + String(defaultSpeedValue))
     }
-    @IBAction func cw(_ sender: Any) {
+    @IBAction func cw(_ sender: Any) {//順時針
         send("cw 45")
     }
-    @IBAction func ccw(_ sender: Any) {
+    @IBAction func ccw(_ sender: Any) {//逆時針
         send("ccw 45")
     }
-    @IBAction func battery(_ sender: Any) {
+    @IBAction func battery(_ sender: Any) {//電量
         send("battery?")
     }
     
+    //設定單機Tello 切換至station mode （連結）指定WiFi
     @IBAction func set_Station_Mode(_ sender: Any) {
         let alert = UIAlertController(title: "填入 ＷiFi名稱 與 密碼", message: "*當需要重新設置 或 發生問題時\n請在開機後長按電源按紐 5秒，即可恢復原廠設置。", preferredStyle: .alert)
+        
+        //SSID & password輸入框
         alert.addTextField { (UITextField) in
             UITextField.placeholder = "SSID"
         }
@@ -307,6 +336,7 @@ class ViewController: UIViewController {
             UITextField.isSecureTextEntry = true
         }
         
+        // ok & cancel 按鍵
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
             guard let ssid = alert.textFields![0].text else{ return}
             guard let password = alert.textFields![1].text else{ return}
@@ -341,6 +371,7 @@ class ViewController: UIViewController {
     }
 
 //=================== read CSV ====================
+    //開啟text選擇器
     @IBAction func readCSV(_ sender: Any) {
         let documentPicker = UIDocumentPickerViewController(documentTypes: ["public.plain-text"], in: .open)
         documentPicker.delegate = self
@@ -348,6 +379,7 @@ class ViewController: UIViewController {
         present(documentPicker, animated: true, completion: nil)
     }
     
+    //CSV字串 to 二維陣列
     func csv_To_Array(_ s: String)->([[String]]){
         var array = [[String]]()
         
@@ -359,6 +391,7 @@ class ViewController: UIViewController {
         return array
     }
 //================== read mp3 =====================
+    //開啟mp3選擇器
     @IBAction func readMusic(_ sender: Any) {
         let musicPicker = UIDocumentPickerViewController(documentTypes: ["public.mp3"], in: .open)
         musicPicker.delegate = self
@@ -366,6 +399,7 @@ class ViewController: UIViewController {
         present(musicPicker, animated: true, completion: nil)
     }
 //================== 檢查IP格式 ==========================
+    //是否為ip格式
     func isIP(_ IP:String?) -> Bool{
         guard let IP = IP else { return false}//nil
         
@@ -381,6 +415,7 @@ class ViewController: UIViewController {
         return true
     }
     
+    //確認csv檔 ip格式是否正確
     func check_CSV_IPformat(_ csv:[[String]]) ->Bool{
         var check = true
         
@@ -395,35 +430,41 @@ class ViewController: UIViewController {
         return check
     }
 //===================== 顯示 ===========================
+    //跳出視窗
     func alert(_ string:String?){
         let alert = UIAlertController(title: "訊息窗", message: string, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
     
+    //顯示在textview上（覆蓋）
     func show(_ s:String){
         DispatchQueue.main.async{
             self.logTextView.text = s
         }
     }
+    
+    //顯示在textview（新增在底下）
     func show_add(_ s:String){
         DispatchQueue.main.async {
             self.logTextView.text = (self.logTextView.text ?? "") + "\n" + s
             self.logTextView.scrollRangeToVisible(self.logTextView.selectedRange)
         }
     }
+    
+    //textview 清空
     func log_clear(){
         DispatchQueue.main.async {
             self.logTextView.text = ""
         }
     }
-//=================================================
+//===================== 畫面消失 do ============================
     override func viewDidDisappear(_ animated: Bool) {
         timerStop()
     }
 }
 
-//實作檔案選取
+//===================== 實作檔案選取 ==============================
 extension ViewController: UIDocumentPickerDelegate{
     func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let selectedFileURL = urls.first else{ return}
@@ -435,11 +476,12 @@ extension ViewController: UIDocumentPickerDelegate{
         if fileType == "csv"{
             let s = try! String(contentsOf: selectedFileURL)
             
-            csv = csv_To_Array(s)
+            csv = csv_To_Array(s)//轉陣列
             print(csv)
             create_Tello_UDP()//重新宣告UDP
             recvData()//開啟資料接收
             csvNameLabel.text = selectedFileURL.lastPathComponent//顯示csv檔名
+            
         //*** mp3檔處理 ***
         }else if fileType == "mp3"{
             do {
